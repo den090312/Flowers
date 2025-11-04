1725fde4636150fe77d6693105e497d098e9032e
dm_bolotin <dm_bolotin@protek.ru>
2025-11-04 19:15:22 +0400

    добавил ps1 в список показываемых файлов

--- content.ps1
+++ content.ps1
@@ -39,6 +39,7 @@ $fileTypes = @(
     @{Name = "CONFIG FILES"; Filter = "*.config"; Color = "DarkCyan"},
     @{Name = "POWERSHELL SCRIPTS"; Filter = "*.ps1"; Color = "Red"},
     @{Name = "SLN FILES"; Filter = "*.sln"; Color = "DarkMagenta"},
+	@{Name = "PS1 FILES"; Filter = "*.ps1"; Color = "DarkMagenta"},
     @{Name = "GITIGNORE"; Filter = ".gitignore"; Color = "DarkGray"}
 )
 
