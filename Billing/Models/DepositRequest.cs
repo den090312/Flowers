5e0fe9db20e2b8a6bf2e17019160b83e194fef5d
dm_bolotin <dm_bolotin@protek.ru>
2025-11-11 00:36:23 +0400

    убрал лишнее

--- Billing/Models/DepositRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Billing.Models
-{
-    public class DepositRequest
-    {
-        public long UserId { get; set; }
-        public decimal Amount { get; set; }
-    }
-}
--- content_run.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -NoExit -File "%~dp0content.ps1"
-pause
